24f639077608a5c1ae0c7b2f940cfcf835f34e12
zhengjunyuan <zhengjunyuan@peilian.com>
2018-12-26 19:14:13 +0800

    mail del

--- console/config/dev.php
+++ console/config/dev.php
@@ -3,12 +3,6 @@ defined('YII_DEBUG') or define("YII_DEBUG", true);
 
 $initConfig = [
     "components"  =>  [
-        'errorHandler'  =>  [
-            "sendTo"   =>  ["xxx1@demo.com","xxx2@demo.com"],
-            "sendCC"    =>  [
-                "xxxx@demo.com"=>"xxxx",
-            ],
-        ],
     ],
     "params"    =>  [
     ]
--- console/config/prod.php
+++ console/config/prod.php
@@ -3,12 +3,6 @@ defined('YII_DEBUG') or define("YII_DEBUG", false);
 
 $initConfig = [
     "components"  =>  [
-        'errorHandler'  =>  [
-            "sendTo"   =>  ["xxx1@demo.com","xxx2@demo.com"],
-            "sendCC"    =>  [
-                "xxxx@demo.com"=>"xxxx",
-            ],
-        ],
     ],
     "params"    =>  [
     ]
